40b9d960ebec7175e2374797efe65c99525beffd
zyloxweeb <giuseppefattor@gmail.com>
2024-05-13 10:03:05 +0200

    Added Favicon🎨

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Benvenuto</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../icon/icon.ico">
+
 </head>
 <body class="bg-gray-900 text-white">
     <div class="flex flex-col justify-center items-center h-screen">
